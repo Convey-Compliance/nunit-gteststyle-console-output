9b02d929bf727eee43109fab95dbbe7a0c10530a
Jose Sebastian Battig <jsbattig@convey.com>
2015-11-17 10:51:19 -0600

    B-0 finished adding more tests

--- test/nunit-gstyle-demo/DemoGTestStyleOutput.cs
+++ test/nunit-gstyle-demo/DemoGTestStyleOutput.cs
@@ -1,21 +1,57 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Threading;
+using NUnit.Framework;
 
-/*[assembly: GTestStyleConsoleOutputAttribute]*/
+[assembly: GTestStyleConsoleOutputAttribute]
 namespace nunit_gstyle_demo
 {
   [TestFixture, GTestStyleConsoleOutputAttribute]
-  class SampleGTestStyleTest
+  public class SampleGTestStyleTest
   {
     [Test]
     public void SimpleTest()
     {
+      Thread.Sleep(200);
       Assert.IsTrue(true);
     }
 
     [Test]
     public void SimpleFailedTest()
     {
+      Thread.Sleep(500);
       Assert.IsTrue(false);
+    }
+
+    [Test]
+    public void SimpleErrorTest()
+    {
+      Thread.Sleep(100);
+      throw new Exception();
+    }
+  }
+
+  [TestFixture, GTestStyleConsoleOutputAttribute]
+  public class SampleGTestStyleTest2
+  {
+    [Test]
+    public void SimpleTest()
+    {
+      Thread.Sleep(200);
+      Assert.IsTrue(true);
+    }
+
+    [Test]
+    public void SimpleFailedTest()
+    {
+      Thread.Sleep(500);
+      Assert.IsTrue(false);
+    }
+
+    [Test]
+    public void SimpleErrorTest()
+    {
+      Thread.Sleep(100);
+      throw new Exception();
     }
   }
 }
